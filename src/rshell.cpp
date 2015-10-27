be1ec38c25a9201c834b5af7933a72f6bcb1b73a
rhale002 <rhale002@ucr.edu>
2015-10-27 22:20:42 +0000

    Made finding the commands and connectors into functions which store each in their own queues

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -6,47 +6,85 @@
 
 using namespace std;
 
-void rshell()
+//Function which finds all the connectors in a char string and puts
+//them in a queue
+queue<char> findConnectors(char cmdCharString[])
 {
-    //Output Command prompt
-    cout << "$ ";
-    
-    //Take in command
-    string cmdString;
-    getline(cin, cmdString);
-    char cmdCharString[cmdString.size() + 1];
-    strcpy(cmdCharString, cmdString.c_str());
-    
-    //Print endl for when TESTING with file
-    cout << endl;
-    
     queue<char> connectorQueue;
+    //Create a pointer pointing to the first occurance of a connector
     char* connectorPointer = strpbrk(cmdCharString, "|;&");
+    //Create a bool for keeping track of doubles
+    bool conDoubCatcher = false;
+    //Loop to find all connectors and put them in the queue
     while (connectorPointer != NULL)
     {
-        //Push connector character into command queue
-        connectorQueue.push(*connectorPointer);
-        
-        // //Print what is put into command queue for TESTING
-        // cout << connectorQueue.back() << endl;
+        //Make sure only one character is stored per connector
+        if(!conDoubCatcher)
+        {
+            //Push connector character into command queue
+            connectorQueue.push(*connectorPointer);
+            if(*connectorPointer == '&' || *connectorPointer == '|')
+                conDoubCatcher = true;
+                
+            // //Print what is put into command queue for TESTING
+            // cout << connectorQueue.back() << endl;
+        }
+        else
+        {
+            conDoubCatcher = false;
+        }
         
         //Go to the next occurance of a connector character
         connectorPointer = strpbrk(connectorPointer + 1, "|;&");
     }
     
+    //return queue with all found connectors
+    return connectorQueue;
+}
+
+queue<char*> findCommands(char cmdCharString[])
+{
+    queue<char*> commandQueue;
     //Create pointer to begining of first command
     char* cmdCharPointer = strtok(cmdCharString, "|;&");
-    //Go through command cString
+    //Go through command cString and find commands
     while(cmdCharPointer != NULL)
     {
         //Get rid of space in begining of command
         while(*cmdCharPointer == ' ')
             cmdCharPointer++;
         
+        commandQueue.push(cmdCharPointer);
+        
         // // Print command for TESTING
-        // printf ("%s\n",cmdCharPointer);
+        // cout << commandQueue.back() << endl;
         
         //Go to next command
         cmdCharPointer = strtok (NULL, "|;&");
     }
+    
+    return commandQueue;
+}
+
+
+//Function to run the shell
+void rshell()
+{
+    //Output Command prompt
+    cout << "$ ";
+    
+    //Take in command and store it in a c-string
+    string cmdString;
+    getline(cin, cmdString);
+    char cmdCharString[cmdString.size()];
+    strcpy(cmdCharString, cmdString.c_str());
+    
+    //Print endl for when TESTING with file
+    cout << endl;
+    
+    //Create a queue filled with connectors in order using findConnectors()
+    queue<char> connectorQueue = findConnectors(cmdCharString);
+    
+    //Create a queue filled with commands in order using findCommands()
+    queue<char*> commandQueue = findCommands(cmdCharString);
 }
--- src/rshell.h
+++ src/rshell.h
@@ -1,6 +1,16 @@
 #ifndef __RSHELL_H__ 
 #define __RSHELL_H__
 
+#include <queue>
+
+//Function which finds all the connectors in a c-string and puts them in a queue
+std::queue<char> findConnectors(char cmdCharString[]);
+
+//Function which finds all the commands in a c-string and puts them in a queue
+std::queue<char*> findCommands(char cmdCharString[]);
+
+//Function to run the shell
 void rshell();
+
 
 #endif
