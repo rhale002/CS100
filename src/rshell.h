143fb9e857b50f842343abf867313046e651abb2
rhale002 <rhale002@ucr.edu>
2015-10-29 00:17:41 +0000

    Created classes for connectors and started work on figuring out how to run commands

--- src/rshell.h
+++ src/rshell.h
@@ -5,9 +5,73 @@
 #include <cstring>
 #include <cstdio>
 #include <queue>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "rshell.h"
 
 using namespace std;
+
+//Virtual class for connectors
+class connector
+{
+    protected:
+        //Store whether or not the last command was successful
+        bool ynSuccess;
+        
+    public:
+        connector(bool ynSuccess)
+        :ynSuccess(ynSuccess)
+        {}
+    
+        virtual bool isGoodOrNot() = 0;
+};
+
+//Class which inherits from connector made for &&
+class andConnector : public connector
+{
+    public:
+        andConnector(bool ynSuccess)
+        : connector(ynSuccess)
+        {}
+        
+        //If last command was successful then return true else return false
+        bool isGoodOrNot()
+        {
+            return ynSuccess;
+        }
+};
+
+//Class which inherits from connector made for ||
+class orConnector : public connector
+{
+    public:
+        orConnector(bool ynSuccess)
+        : connector(ynSuccess)
+        {}
+        
+        //If last command was successful then return false else return true
+        bool isGoodOrNot()
+        {
+            return !ynSuccess;
+        }
+};
+
+//Class which inherits from connector made for ;
+class semiColonConnector : public connector
+{
+    public:
+        semiColonConnector(bool ynSuccess)
+        : connector(ynSuccess)
+        {}
+        
+        //Always return true;
+        bool isGoodOrNot()
+        {
+            return true;
+        }
+};
 
 //Function which finds all the connectors in a char string and puts
 //them in a queue
@@ -15,11 +79,12 @@ queue<char> findConnectors(char cmdCharString[])
 {
     queue<char> connectorQueue;
     //Create a pointer pointing to the first occurance of a connector
-    char* connectorPointer = strpbrk(cmdCharString, "|;&");
+    char* connectorPointer = strpbrk(cmdCharString, "|;&#");
     //Create a bool for keeping track of doubles
     bool conDoubCatcher = false;
+    bool hashCatcher = false;
     //Loop to find all connectors and put them in the queue
-    while (connectorPointer != NULL)
+    while (connectorPointer != NULL && hashCatcher == false)
     {
         //Make sure only one character is stored per connector
         if(!conDoubCatcher)
@@ -28,6 +93,8 @@ queue<char> findConnectors(char cmdCharString[])
             connectorQueue.push(*connectorPointer);
             if(*connectorPointer == '&' || *connectorPointer == '|')
                 conDoubCatcher = true;
+            else if(*connectorPointer == '#')
+                hashCatcher = true;
                 
             // //Print what is put into command queue for TESTING
             // cout << connectorQueue.back() << endl;
@@ -38,7 +105,7 @@ queue<char> findConnectors(char cmdCharString[])
         }
         
         //Go to the next occurance of a connector character
-        connectorPointer = strpbrk(connectorPointer + 1, "|;&");
+        connectorPointer = strpbrk(connectorPointer + 1, "|;&#");
     }
     
     //return queue with all found connectors
@@ -49,7 +116,7 @@ queue<char*> findCommands(char cmdCharString[])
 {
     queue<char*> commandQueue;
     //Create pointer to begining of first command
-    char* cmdCharPointer = strtok(cmdCharString, "|;&");
+    char* cmdCharPointer = strtok(cmdCharString, "|;&#");
     //Go through command cString and find commands
     while(cmdCharPointer != NULL)
     {
@@ -63,11 +130,25 @@ queue<char*> findCommands(char cmdCharString[])
         // cout << commandQueue.back() << endl;
         
         //Go to next command
-        cmdCharPointer = strtok (NULL, "|;&");
+        cmdCharPointer = strtok (NULL, "|;&#");
     }
     
     return commandQueue;
 }
+
+//Function which seperates the command from other stuff and returns the command
+char* createCommand(char* cmdCharPointer)
+{
+    //Seperate command from other stuff
+    cmdCharPointer = strtok(cmdCharPointer, " ");
+    
+    // //Used for TESTING if command was separated properly
+    // cout << cmdCharPointer << endl;
+    
+    //return command
+    return cmdCharPointer;
+}
+
 
 
 //Function to run the shell
@@ -86,10 +167,45 @@ void rshell()
     cout << endl;
     
     //Create a queue filled with connectors in order using findConnectors()
-    queue<char> connectorQueue = findConnectors(cmdCharString);
+    queue<char> connectorCharQueue = findConnectors(cmdCharString);
     
     //Create a queue filled with commands in order using findCommands()
     queue<char*> commandQueue = findCommands(cmdCharString);
+    
+    // //Create bool to store whether command execution was a success
+    // bool ynSuccess = true;
+    
+    // //Run commands until we run out of commands to call
+    // while(!commandQueue.empty())
+    // {
+    //     pid_t pid;
+    //     int status;
+        
+    //     //Prepare command by seperating arguments from command
+        
+        
+    //     //Run fork and get pid
+    //     //If an error while forking occurs then say so and exit
+    //     if ((pid = fork()) < 0) 
+    //     {
+    //         cout << "ERROR: forking child process failed" << endl;
+    //         exit(1);
+    //     }
+    //     else if (pid == 0) 
+    //     {
+    //         //Run the command and record whether it was successful
+    //         if(execvp(*argv, argv) < 0)
+    //             ynSuccess = false;
+    //         else
+    //             ynSuccess = true;
+    //     }
+    //     else 
+    //     {
+    //         //Wait for completion
+    //         while (wait(&status) != pid);
+            
+    //     }
+    // }
     
     delete[] cmdCharString;
 }
