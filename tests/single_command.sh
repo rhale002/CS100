456685fc73b49f364c4495c249d861a556526c2e
rhale002 <rhale002@ucr.edu>
2015-11-02 22:42:27 +0000

    Added actual test cases from project specs. Only starting writing first one

---
+++ tests/single_command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#tests single commands
+echo 'ls' | bin/rshell
+echo 'ls -a' | bin/rshell
+echo 'ls -al' | bin/rshell
+echo 'echo hello' | bin/rshell
+echo 'echo hello world' | bin/rshell
+echo 'echo hello world! This' | bin/rshell
+echo 'echo hello world! This program' | bin/rshell
+echo 'echo Hello world! This program works!' | bin/rshell
+echo 'date' | bin/rshell
+echo 'cal' | bin/rshell
+echo 'mkdir DeleteMe' | bin/rshell
+echo 'echo DeleteMe Directory created' | bin/rshell
+echo 'rmdir DeleteMe' | bin/rshell
+echo 'echo DeleteMe Directory removed' | bin/rshell
