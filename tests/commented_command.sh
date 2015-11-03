81ed7208f09af94843f05255534c24f31027f960
rhale002 <rhale002@ucr.edu>
2015-11-03 23:02:51 +0000

    Added some tests to exit and commenting and found errors in commenting

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -4,3 +4,12 @@
 echo 'ls -a; echo hello&& mkdir DELETE # echo world; git status' | bin/rshell
 echo '#ls -a; echo hello&& mkdir DELETE|| echo world; git status' | bin/rshell
 echo '   #ls -a; echo hello&& mkdir DELETE|| echo world; git status' |bin/rshell
+echo 'ls -a; echo hello&& mkdir DELETE|| echo world #git status' |bin/rshell
+echo 'ls -a; echo hello&& mkdir DELETE|| echo world; git status#' |bin/rshell
+echo 'ls -a; echo hello&& mkdir DELETE # echo world; git status' | bin/rshell
+
+#Having hash signs in echo arguments is cray. Otherwise works.
+echo 'ls -a; echo hello# mkdir DELETE|| echo world; git status' |bin/rshell
+
+#Doesn't give error for hash being next to argument
+echo 'ls -a# echo hello&& mkdir DELETE|| echo world; git status' | bin/rshell
--- tests/exit.sh
+++ tests/exit.sh
@@ -2,3 +2,6 @@
 
 #tests exit and commands with exit
 echo 'ls -a; echo hello&& exit || echo world; git status' | bin/rshell
+echo 'exit; ls -a;echo hello&& mkdir DELETE||echo world;git status' | bin/rshell
+echo 'ls -a; echo hello&& mkdir DELETE&& exit; git status' | bin/rshell
+echo 'ls -a; echo hello&& mkdir DELETE|| echo world; exit' | bin/rshell
