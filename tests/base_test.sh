3fe4bc1515849e6ca9c0244763c542b94769f07d
rhale002 <rhale002@ucr.edu>
2015-11-22 19:41:45 +0000

    Turned all the tests into script and finished code

--- tests/base_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Base test
-echo --------------------------------------------------------------------------
-echo Base test
-echo --------------------------------------------------------------------------
-echo 'ls -a; echo hello&& mkdir DELETE|| echo world; git status' | bin/rshell
